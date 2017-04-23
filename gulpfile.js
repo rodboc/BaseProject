d7cb3c2c48aa92719cd3ecd9f16b20840dc1ffa8
Rodrigo Acevedo <hola@rodrigo-acevedo.com>
2017-04-23 23:53:11 +1200

    Sourcemaps

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,18 @@
-var gulp        = require('gulp');
+var gulp = require('gulp');
 var browserSync = require('browser-sync').create();
-var sass        = require('gulp-sass');
+var sass = require('gulp-sass');
+var sourcemaps = require('gulp-sourcemaps');
+
+var input = './dev/resources/scss/*.scss';
+var output = './dev/assets/css';
+
+var sassOptions = {
+    errLogToConsole: true,
+    outputStyle: 'expanded'
+};
 
 // Static Server + watching scss/html files
-gulp.task('serve', ['sass'], function() {
+gulp.task('serve', ['sass'], function () {
 
     browserSync.init({
         server: "./dev"
@@ -14,9 +23,12 @@ gulp.task('serve', ['sass'], function() {
 });
 
 // Compile sass into CSS & auto-inject into browsers
-gulp.task('sass', function() {
+gulp.task('sass', function () {
     return gulp.src("dev/resources/scss/*.scss")
         .pipe(sass())
+        .pipe(sourcemaps.init())
+        .pipe(sass(sassOptions).on('error', sass.logError))
+        .pipe(sourcemaps.write("./maps"))
         .pipe(gulp.dest("dev/assets/css"))
         .pipe(browserSync.stream());
 });
